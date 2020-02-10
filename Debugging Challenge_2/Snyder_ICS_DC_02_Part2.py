ae4dfbcd4046b848beb12dec56a7ff3471cc609b
AlemSnyder <44094752+AlemSnyder@users.noreply.github.com>
2020-02-10 13:44:29 -0600

    move file out of folder

--- Debugging Challenge_2/Snyder_ICS_DC_02_Part2.py
+++ Debugging Challenge_2/Snyder_ICS_DC_02_Part2.py
@@ -4,9 +4,9 @@
 #the recurcive definit of a sumation
 def sumation(function, start, stop):
     if start == stop:
-        return function(star)
+        return function(start)
     else:
         total = function(start) + sumation(function, start+1, stop)
     return total
 
-print(sumation(lambda x : x*x, 0,100))
+print(sumation(lambda x : 1/(100-x), 0,100))
